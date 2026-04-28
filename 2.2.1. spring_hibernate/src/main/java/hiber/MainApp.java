2112d5644b44b345dc6358aa47d0fb473ca6b7fc
Дойко Игорь <igor_do@mail.ru>
2026-04-28 22:58:19 +0300

    Задача выполнена. Создал сущность Car Создал связь one to one юзера с машиной добавил класс в настройки Сделал загрузку выгрузку пользователей с машинами добавил метод выгрузки юзера по запросу

--- 2.2.1. spring_hibernate/src/main/java/hiber/MainApp.java
+++ 2.2.1. spring_hibernate/src/main/java/hiber/MainApp.java
@@ -1,34 +1,40 @@
 package hiber;
 
 import hiber.config.AppConfig;
+import hiber.model.Car;
 import hiber.model.User;
 import hiber.service.UserService;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
+import javax.jws.soap.SOAPBinding;
 import java.sql.SQLException;
 import java.util.List;
 
 public class MainApp {
-   public static void main(String[] args) throws SQLException {
-      AnnotationConfigApplicationContext context = 
-            new AnnotationConfigApplicationContext(AppConfig.class);
+    public static void main(String[] args) throws SQLException {
+        AnnotationConfigApplicationContext context =
+                new AnnotationConfigApplicationContext(AppConfig.class);
 
-      UserService userService = context.getBean(UserService.class);
+        UserService userService = context.getBean(UserService.class);
 
-      userService.add(new User("User1", "Lastname1", "user1@mail.ru"));
-      userService.add(new User("User2", "Lastname2", "user2@mail.ru"));
-      userService.add(new User("User3", "Lastname3", "user3@mail.ru"));
-      userService.add(new User("User4", "Lastname4", "user4@mail.ru"));
+        userService.add(new User("userWithMersedes", "Lastname5", "user5@mail.ru", new Car("Mersedes", 777)));
+        userService.add(new User("userWithAudi", "Lastname6", "user6@mail.ru", new Car("Audi", 333)));
+        userService.add(new User("userWithToyota", "Lastname7", "user7@mail.ru", new Car("Toyota", 111)));
+        userService.add(new User("userWithAudi2", "Lastname7", "user7@mail.ru", new Car("Audi", 666)));
 
-      List<User> users = userService.listUsers();
-      for (User user : users) {
-         System.out.println("Id = "+user.getId());
-         System.out.println("First Name = "+user.getFirstName());
-         System.out.println("Last Name = "+user.getLastName());
-         System.out.println("Email = "+user.getEmail());
-         System.out.println();
-      }
+        List<User> users = userService.listUsers();
+        for (User user : users) {
+            System.out.println("Id = " + user.getId());
+            System.out.println("First Name = " + user.getFirstName());
+            System.out.println("Last Name = " + user.getLastName());
+            System.out.println("Email = " + user.getEmail());
+            System.out.println("Model = " + user.getCar().getModel());
+            System.out.println("Series = " + user.getCar().getSeries());
+            System.out.println();
+        }
 
-      context.close();
-   }
+        User user = userService.getUser("Audi",333);
+        System.out.println(user.getFirstName());
+        context.close();
+    }
 }
--- 2.2.1. spring_hibernate/src/main/java/hiber/dao/UserDaoImp.java
+++ 2.2.1. spring_hibernate/src/main/java/hiber/dao/UserDaoImp.java
@@ -1,29 +1,37 @@
 package hiber.dao;
 
+import hiber.model.Car;
 import hiber.model.User;
 import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
+import javax.jws.soap.SOAPBinding;
 import javax.persistence.TypedQuery;
 import java.util.List;
 
 @Repository
 public class UserDaoImp implements UserDao {
 
-   @Autowired
-   private SessionFactory sessionFactory;
+    @Autowired
+    private SessionFactory sessionFactory;
 
-   @Override
-   public void add(User user) {
-      sessionFactory.getCurrentSession().save(user);
-   }
+    @Override
+    public void add(User user) {
+        sessionFactory.getCurrentSession().save(user);
+    }
 
-   @Override
-   @SuppressWarnings("unchecked")
-   public List<User> listUsers() {
-      TypedQuery<User> query=sessionFactory.getCurrentSession().createQuery("from User");
-      return query.getResultList();
-   }
+    @Override
+    @SuppressWarnings("unchecked")
+    public List<User> listUsers() {
+        TypedQuery<User> query = sessionFactory.getCurrentSession().createQuery("from User");
+        return query.getResultList();
+    }
 
+    @Override
+    @SuppressWarnings("unchecked")
+    public User getUser(String model, int series) {
+        return sessionFactory.getCurrentSession().createQuery("SELECT s FROM User s WHERE s.car.model = :model AND s.car.series = :series", User.class)
+                .setParameter("model", model).setParameter("series", series).getSingleResult();
+    }
 }
